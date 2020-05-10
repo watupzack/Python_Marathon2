47bfa7d21fa3842e518ce592ce7cf2a1e03ec8d8
Maxim Rezan <tvletsplay412@gmail.com>
2020-05-10 09:20:19 +0300

    add, sub

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 print("User")
+
+number_one = ""
+number_two = ""
+
+while number_one == "" or number_two == "":
+    try:
+        number_one = int(input("Введите первое неотрицательное число: "))
+        if number_one < 0:
+            raise ValueError("ValueError exception thrown")
+
+        number_two = int(input("Введите второе неотрицательное число: "))
+        if number_two < 0:
+            raise ValueError("ValueError exception thrown")
+
+        # Addition and subtraction
+        print(f"Результат сложения = {number_one+number_two}")
+        print(f"Результат вычитания = {number_one-number_two}")
+
+    except ValueError:
+        number_one = ""
+        number_two = ""
+        print("Ничего кроме неотрицательных чисел!")
+        print("Заново!")
